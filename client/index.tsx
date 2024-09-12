3fd1d50cbff1bf66c99466eb9555133da25b18a1
ADC-RACC <alexanderclarknz@gmail.com>
2024-09-13 08:09:30 +1200

    Initial commit

---
+++ client/index.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router-dom'
+
+import router from './router.tsx'
+
+const queryClient = new QueryClient()
+
+document.addEventListener('DOMContentLoaded', () => {
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>,
+  )
+})
---
+++ client/router.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-key */
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
+import App from './components/App'
+
+const router = createBrowserRouter(
+  createRoutesFromElements([<Route path="/" element={<App />} />])
+)
+
+export default router
